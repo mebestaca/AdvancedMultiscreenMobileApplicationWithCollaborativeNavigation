b6d7cfc2d6f535f66355f886c75ba090e4e10d36
Marc Edison Estaca <mebestaca@gmail.com>
2026-06-10 23:26:45 -0600

    added setup for tab navigation

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,9 +1,9 @@
 import { StyleSheet, Text, View } from "react-native";
 
-export default function Index() {
+export default function Tab() {
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <Text>Tab [Home|Settings]</Text>
     </View>
   );
 }
@@ -11,7 +11,7 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
   },
 });
