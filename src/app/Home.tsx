2cf5921b8b66fbc7144b085c3c82f52ba49dc870
reinerjustin <reinerjustin.realica@edu.sait.ca>
2026-06-17 08:23:42 -0600

    homepage

---
+++ src/app/Home.tsx
@@ -0,0 +1,154 @@
+import React from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { styles } from "../styles/home.styles";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.tabs}>
+          <View style={styles.tab}>
+            <Text style={styles.activeText}>Today</Text>
+            <View style={styles.underline} />
+          </View>
+
+          <View style={styles.tab}>
+            <Text style={styles.inactiveText}>Community</Text>
+          </View>
+        </View>
+
+        <View style={styles.headerIcons}>
+          <View style={styles.iconWithText}>
+            <Ionicons name="flash-outline" size={22} color="#000" />
+            <Text style={styles.iconText}>2</Text>
+          </View>
+
+          <Ionicons name="notifications-outline" size={22} color="#000" />
+        </View>
+    </View>
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.card}>
+          <View style={styles.cardRow}>
+            <View style={styles.cardContent}>
+              <Text style={styles.cardTitle}>
+                🏆 This World Cup...
+              </Text>
+
+              <Text style={styles.cardText}>
+                Take on the Mid-Year Challenge, complete a 7-day streak, and help
+                your country win.
+              </Text>
+
+              <View style={styles.cardButtons}>
+                <Text style={styles.joinBtn}>Join Now</Text>
+                <Text style={styles.dismissBtn}>Dismiss</Text>
+              </View>
+            </View>
+
+            <Image
+              source={{
+                 uri: "https://images.unsplash.com/photo-1521412644187-c49fa049e84d",
+              }}
+              style={styles.cardImage}
+            />
+
+           </View>
+
+        </View>
+
+        <Text style={styles.greeting}>Good evening</Text>
+        <ImageBackground
+          source={{
+            uri: "https://images.unsplash.com/photo-1514477917009-389c76a86b68?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHNreXxlbnwwfHwwfHx8MA%3D%3D",
+          }}
+          style={styles.verseCard}
+          imageStyle={{ borderRadius: 16 }}
+        >
+          <View style={styles.topRow}>
+            <View>
+              <Text style={styles.smallText}>Verse of the day</Text>
+              <Text style={styles.verseTitle}>Micah 6:8 NIV</Text>
+            </View>
+          </View>
+          
+          <View style={styles.middle}>
+            <Text style={styles.verseText}>
+              He has shown you, O mortal, what is good. And what does the LORD 
+              require of you? To act justly and to love mercy and to walk humbly with 
+              your God.
+            </Text>
+          </View>
+
+          <View style={styles.actions}>
+            <TouchableOpacity style={styles.actionBtn}>
+              <Ionicons name="heart-outline" size={20} color="#fff" />
+              <Text style={styles.actionText}>1.58M</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.actionBtn}>
+              <Ionicons name="chatbubble-outline" size={20} color="#fff" />
+              <Text style={styles.actionText}>26,098</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.actionBtn}>
+              <Ionicons name="share-outline" size={20} color="#fff" />
+              <Text style={styles.actionText}>556k</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.actionBtn}>
+              <Ionicons name="ellipsis-horizontal" size={20} color="#fff" />
+              <Text style={styles.actionText}>More</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.bottomRow}>
+            <View style={{width:20}} />
+            <View style={styles.bottomLeft}>
+              <Ionicons name="notifications-outline" size={18} color="#000" />
+              <Text style={styles.bottomText}>Send me this daily</Text>
+            </View>
+            <TouchableOpacity>
+              <Ionicons name="close-outline" size={20} color="#000" />
+            </TouchableOpacity>
+          </View>
+        </ImageBackground>
+      </ScrollView>
+
+      <View style={styles.footer}>
+        <View style={styles.footerItem}>
+          <Ionicons name="home" size={22} color="#000" />
+          <Text style={styles.footerText}>Home</Text>
+        </View>
+
+        <View style={styles.footerItem}>
+          <Ionicons name="book-outline" size={22} color="#999" />
+          <Text style={styles.footerTextInactive}>Bible</Text>
+        </View>
+
+        <View style={styles.footerItem}>
+          <Ionicons name="list-outline" size={22} color="#999" />
+          <Text style={styles.footerTextInactive}>Plans</Text>
+        </View>
+
+        <View style={styles.footerItem}>
+          <Ionicons name="compass-outline" size={22} color="#999" />
+          <Text style={styles.footerTextInactive}>Discover</Text>
+        </View>
+
+        <View style={styles.footerItem}>
+          <Ionicons name="person-outline" size={22} color="#999" />
+          <Text style={styles.footerTextInactive}>You</Text>
+        </View>
+      </View>
+</View>
+  );
+}
---
+++ src/styles/home.styles.ts
@@ -0,0 +1,239 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f6f8",
+  },
+  
+  header: {
+    paddingTop: 50,
+    paddingHorizontal: 16,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-end",
+    backgroundColor: "#fff",
+    paddingBottom: 10,
+  },  
+
+    tabs: {
+      flexDirection: "row",
+      gap: 20,
+      alignItems: "flex-end",
+    },
+    
+    tab: {
+      alignItems: "center",
+    },
+    
+    activeText: {
+      fontSize: 20,
+      fontWeight: "700",
+      color: "#000",
+    },
+    
+    inactiveText: {
+      fontSize: 20,
+      fontWeight: "500",
+      color: "#999",
+    },
+    
+    underline: {
+      marginTop: 4,
+      height: 3,
+      width: "100%",
+      backgroundColor: "red",
+      borderRadius: 2,
+    },
+  
+    headerIcons: {
+      flexDirection: "row",
+      alignItems: "center",
+      gap: 15,
+    },
+
+    iconWithText: {
+      flexDirection: "row",
+      alignItems: "center",
+      gap: 4,
+    },
+    
+    iconText: {
+      fontSize: 14,
+      fontWeight: "600",
+      color: "#000",
+    },
+    
+    greeting: {
+      fontSize: 18,
+      fontWeight: "600",
+      margin: 16,
+    },
+  
+    verseCard: {
+      marginHorizontal: 16,
+      padding: 16,
+      borderRadius: 16,
+      overflow: "hidden",
+      justifyContent: "space-between",
+      minHeight: 320,
+    },
+
+    topRow: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "flex-start",
+    },
+
+    middle: {
+      flex: 1,
+      justifyContent: "center",
+    },
+  
+    smallText: {
+      color: "#fff",
+      fontSize: 12,
+      marginBottom: 2
+    },
+  
+    verseTitle: {
+      color: "#fff",
+      fontSize: 18,
+      fontWeight: "700",
+      marginVertical: 0,
+    },
+  
+    verseText: {
+      color: "#fff",
+      fontSize: 20,
+      lineHeight: 22,
+      marginHorizontal: 10,
+    },
+  
+    actions: {
+      flexDirection: "row",
+      marginTop: 15,
+      justifyContent: "center",
+      alignItems: "center",
+      gap: 50
+    },
+  
+    actionBtn: {
+      alignItems: "center",
+      gap: 4,
+    },
+  
+    actionText: {
+      color: "#fff",
+      fontSize: 12,
+    },
+
+    bottomRow: {
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-between",
+      backgroundColor: "#fff",
+      paddingVertical: 8,
+      paddingHorizontal: 12,
+      borderRadius: 20,
+      marginTop: 10,
+    },
+
+    bottomLeft: {
+      flexDirection: "row",
+      alignItems: "center",
+      gap: 6,
+    },
+
+    bottomText: {
+      color: "#000",
+      fontSize: 12,
+      marginLeft: 6,
+    },
+  
+    card: {
+      backgroundColor: "#fff",
+      marginHorizontal: 16,
+      marginTop: 16,
+      padding: 16,
+      borderRadius: 16,
+      shadowColor: "#000",
+      shadowOpacity: 0.05,
+      shadowRadius: 10,
+      elevation: 2,
+    },
+
+    cardRow: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+    },
+    
+    cardContent: {
+      flex: 1,             
+      paddingRight: 12,
+    },
+    
+    cardImage: {
+      width: 80,
+      height: 80,
+      borderRadius: 12,
+    },
+    
+    cardTitle: {
+      fontSize: 16,
+      fontWeight: "700",
+      marginBottom: 8,
+    },
+    
+    cardText: {
+      fontSize: 14,
+      color: "black",
+      fontWeight: "400",
+      lineHeight: 20,
+    },
+    
+    cardButtons: {
+      flexDirection: "row",
+      gap: 10,
+      marginTop: 12,
+    },
+    
+    joinBtn: {
+      color: "black",
+      fontWeight: "600",
+      fontSize: 14,
+    },
+    
+    dismissBtn: {
+      color: "#999",
+      fontSize: 14,
+    },
+
+    footer: {
+      flexDirection: "row",
+      justifyContent: "space-around",
+      alignItems: "center",
+      paddingVertical: 12,
+      backgroundColor: "#fff",
+      borderTopWidth: 1,
+      borderTopColor: "#eee",
+    },
+    
+    footerItem: {
+      alignItems: "center",
+    },
+    
+    footerText: {
+      fontSize: 12,
+      color: "#000",
+      marginTop: 2,
+      fontWeight: "600",
+    },
+    
+    footerTextInactive: {
+      fontSize: 12,
+      color: "#999",
+      marginTop: 2,
+    },
+  });
